549dfc0a1cdff22702698281f213b8011cc6a079
PavelIvasyuk <dotmmoru@gmail.com>
2023-02-07 12:31:13 +0200

    add timer and score texts

--- synk_framework_test_game/Public/Scripts/RPS_GameController.js
+++ synk_framework_test_game/Public/Scripts/RPS_GameController.js
@@ -1,14 +1,24 @@
 // -----JS CODE-----
 //@input SceneObject readyUI
 //@input SceneObject gameplayUI
+//@input Component.Text yourScore
+//@input Component.Text otherScore
+//@input Component.Text timer
 //@input Component.ScriptComponent[] buttons
 
 var selectedBtn = -1;
+
+// NEED TO GET USER NAMES 
+var yourName = "You";
+var otherName = "Other";
 
 function Start()
 {
 	//script.readyUI.enabled = true;
 	script.gameplayUI.enabled = false;
+	SetTimerText("");
+	GetUserNames();
+	SetNamesAndScore(0,0);
 }
 
 script.api.OnClick_ReadyButton = function()
@@ -36,7 +46,21 @@ function UnSelectAllButtons(selected)
 	}
 }
 
+function GetUserNames()
+{
+	yourName = "You";
+	otherName = "Other";
+}
 
+function SetNamesAndScore(scoreYou, scoreOther)
+{
+	script.yourScore.text = yourName + ": " + scoreYou;
+	script.otherScore.text = otherName + ": " + otherScore;
+}
 
+function SetTimerText(value)
+{
+	script.timer.text = value;
+}
 
 Start();
